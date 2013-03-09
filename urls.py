56a3c9883c0c0966fb8fb497fb5f7a8802db8539
Patrick Killian <pkillian@berkeley.edu>
2013-03-08 23:57:03 -0800

    fix dumb typo shit

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
-from eventmanager.api import EntryResource
+from eventmanager.api import EntryResource, OrganizationResource
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
