600409b2a82261e0e6149622599f0a0954238568
Patrick Killian <pkillian@berkeley.edu>
2013-03-09 00:30:16 -0800

    test bugfix

--- eventmanager/models.py
+++ eventmanager/models.py
@@ -5,8 +5,10 @@ class Event(models.Model):
     event_name = models.CharField(max_length=100)
     org_name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
-    start_time = models.DateTimeField(auto_now=True)
-    end_time = models.DateTimeField(auto_now=True)
+    start_date = models.DateField()
+    start_time = models.TimeField()
+    end_date = models.DateField()
+    end_time = models.TimeField()
     
     def __unicode__(self):
         return self.event_name
