6f3320a59d72145fd85697a41ad7363172d6bcec
Patrick Killian <pkillian@berkeley.edu>
2013-03-08 22:23:06 -0800

    Adding tastypie; installed apps change.

--- web/settings.py
+++ web/settings.py
@@ -119,7 +119,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-        'eventmanager'
+    'django-tastypie',
+    'eventmanager'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
